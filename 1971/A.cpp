419609016d44f0af73420ec3b3381af29936fde1
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-04-10 14:16:17 +0600

    1971/A.cpp

---
+++ 1971/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << min(x, y) << " " << max(x, y) << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
