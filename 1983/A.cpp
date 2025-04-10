6c9d5d37084ce3ef745efaad23a6d6ee9a0a8821
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-04-10 14:53:22 +0600

    1983/A.cpp

---
+++ 1983/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+void soln()
+{
+    int n;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cout << i << " ";
+
+    cout << endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+        soln();
+    return 0;
+}
