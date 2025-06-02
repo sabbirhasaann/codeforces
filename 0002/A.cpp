3ff0cfd7dae053b1d3fc37f70723990f17311e90
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-02 17:24:52 +0600

    md and prob a sol

---
+++ 0002/A.cpp
@@ -0,0 +1,22 @@
+/**
+ *    author:  iamsabbir
+ *    created:23-06-2024 02:47pm
+ *    Contest:Codeforces Beta Round 2
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    unordered_map<string, int> mp;
+    while (t--)
+    {
+        string s;
+        int n;
+        cin >> s >> n;
+        mp[s] = n;
+    }
+}
