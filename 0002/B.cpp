9a7d6b968d0634a32afb4b7fe048c3aacb5c0b7e
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-04 19:44:31 +0600

    0002 B - just take input matrix and print it. problem not solved yet

---
+++ 0002/B.cpp
@@ -0,0 +1,40 @@
+// 2025/06/04 17:17:10
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+
+vector<vector<int>> input_matrix(int n, int m)
+{
+    vector<vector<int>> arr(n, vector<int>(m));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> arr[i][j];
+    return arr;
+}
+
+void print_matrix(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    int m = matrix[0].size();
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cout << matrix[i][j] << " ";
+        cout << endl;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<vector<int>> matrix = input_matrix(n, n);
+    
+}
