7c4a5111d7acbf68e0f2251d8f122a371d239ce3
sabbirhasaann <ce2100@mbstu.ac.bd>
2025-06-11 11:36:53 +0600

    2072 C trying solve

---
+++ 2072/B.cpp
@@ -0,0 +1,23 @@
+// 2025/06/11 10:23:01
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+void soln()
+{
+    
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
