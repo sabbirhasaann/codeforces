265be44f5a1237f32ad3aef4cea363df28ee3415
sabbirhasaann <sabbirhasan9989@gmail.com>
2025-07-30 20:09:38 +0600

    1875 c jellyfish and green apples solved

--- 1875/C.cpp
+++ 1875/C.cpp
@@ -1,74 +1,33 @@
 #include <iostream>
 #include <bits/stdc++.h>
 #define endl "\n"
+#define ll long long
 using namespace std;
-void solve();
-void small(int n,int m);
-void large(int n,int m);
+
+ll solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    n %= m;
+    int g = __gcd(n, m);
+    int a = n / g;
+    int b = m / g;
+
+    if (n == 0)
+        return 0;
+
+    if (__builtin_popcount(b) > 1)
+        return -1;
+
+    return 1ll * __builtin_popcount(a) * m - n;
+}
 
 int main()
 {
     int t;
     cin >> t;
 
-    while(t--)
-        solve();
-}
-
-void solve()
-{
-    int n,m; //n is the number of apple piecess and m is the number of people
-    cin >> n >> m;
-    if(n==m)
-    {
-        cout << 0 <<endl;
-        return;
-    }
-    if(n>m)
-        large(n,m);
-    else
-        small(n,m);
-
-}
-
-void small(int n,int m)
-{
-    int rem = n,counter=0;
-    while(rem%m!=0)
-    {
-        if(rem==1)
-        {
-            cout << -1 << endl;
-            return;
-        }
-        rem*=2;
-        counter++;
-        if(rem>=m)
-            rem%=m;
-    }
-    cout << counter << endl;
-}
-
-void large(int n,int m)
-{
-    if(n%m==0)
-    {
-        cout << 0 << endl;
-        return;
-    }
-
-    int rem = n%m,counter=1;
-    while(rem%m!=0)
-    {
-        if(rem==1)
-        {
-            cout << -1 << endl;
-            return;
-        }
-        rem*=2;
-        counter++;
-        if(rem>=m)
-            rem%=m;
-    }
-    cout << counter << endl;
+    while (t--)
+        cout << solve() << endl;
 }
