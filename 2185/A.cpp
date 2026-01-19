09847baf69bf9c062fe955bb1e987ecd43856b1f
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-01-19 12:47:30 +0600

    Contest Time Solved - 2185A Perfect Root

---
+++ 2185/A.cpp
@@ -0,0 +1,28 @@
+// 2026/01/18 20:10:47
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+void soln()
+{
+    int n;
+    cin >>n;
+
+    for(int i=1;i<=n;i++)
+        cout << i << " ";
+    cout << endl;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
