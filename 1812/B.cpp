c52ec91280ac9fb4eeb14261d1144604fdfd7392
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-01-24 21:24:26 +0600

    Upsolved 1812B Was it Rated?

---
+++ 1812/B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    if(n == 15 || n == 20 || n == 21)
+        cout << "NO\n";
+    else
+        cout <<"YES\n";   
+}
