6d143b9243a010b6e65fb39da648edbd1f421e29
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-01-24 21:25:25 +0600

    Upsolved - 1812A Are You A Robot?

---
+++ 1812/A.cpp
@@ -0,0 +1,21 @@
+/*	nrooted
+	08:59pm
+ 01-04-2023*/
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <map>
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+
+	cout <<"security\n";
+	return 0;
+}
