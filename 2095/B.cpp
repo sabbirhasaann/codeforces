a6c8d8a442b71ec4308f2a721e005850f6780227
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-01 14:46:45 +0600

    Upsolved 2095B. Plinko

---
+++ 2095/B.cpp
@@ -0,0 +1,20 @@
+// 2025/09/03 22:52:06
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+void soln()
+{
+    cout << "-1\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    soln();
+    return 0;
+}
