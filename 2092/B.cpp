442dedec7667cdb8f26fc87062ed220f5a280d8e
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-03 19:47:50 +0600

    added efficient solution

--- 2092/B.cpp
+++ 2092/B.cpp
@@ -4,6 +4,23 @@
 using namespace std;
 #define ll long long
 #define endl '\n'
+
+void solve() {
+    int n; cin >> n;
+    string a, b; cin >> a >> b;
+    int cnt1 = 0, cnt2 = 0;
+    for (int i = 0; i < n; ++i) {
+        if (i & 1) {
+            cnt2 += a[i] == '0';
+            cnt1 += b[i] == '0';
+        } else {
+            cnt1 += a[i] == '0';
+            cnt2 += b[i] == '0';
+        }
+    }
+    cout << (cnt1 >= (n + 1) / 2 && cnt2 >= n / 2 ? "Yes" : "No") << '\n';
+}
+
 void soln()
 {
     int n,odd1=0,even1=0,odd0=0,even0=0;
