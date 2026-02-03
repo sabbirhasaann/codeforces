e61cc340472317dc78201c3dab6b129efe2f67cc
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-03 16:45:38 +0600

    added efficient soln

--- 0914/A.cpp
+++ 0914/A.cpp
@@ -1,6 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void efficientSoln(){
+    long long ans=LLONG_MIN, n, x;
+    cin>>n;
+    for (long long i = 0; i < n; i++)
+    {
+        cin>>x;
+        for (long long j = 0; j*j<=x; j++)
+            if (j*j == x)
+                x = LLONG_MIN;
+        ans = max(ans, x);
+    }
+    cout << ans << endl;
+}
 
 int main()
 {
