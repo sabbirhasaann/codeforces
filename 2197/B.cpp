f83eb68a6869fef21fad1d09862757979e52b366
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-20 16:14:07 +0600

    soln 1&2

--- 2197/B.cpp
+++ 2197/B.cpp
@@ -23,7 +23,7 @@ int removeConsecutiveDuplicateInPlace(vector<int> &a){
     
     return k;
 }
-void soln()
+void soln() // two pointer base solution
 {
     int n;
     cin >> n;
@@ -51,6 +51,64 @@ void soln()
         cout <<"NO\n";
 }   
 
+void soln1(){ // poistion mapping based solution
+    int n;
+    cin >> n;
+    vector<int> p(n), a(n),pos(n + 1);
+    for(int i=0;i<n;i++){
+        cin >> p[i];
+        pos[p[i]]= i;
+    }
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+    for(int i=0;i<n-1;++i){
+        if(a[i] != a[i+1])
+        {
+            if(pos[a[i]]>pos[a[i+1]]){
+                cout <<"NO\n";
+                return;
+            }
+        }
+    }
+    cout <<  "YES\n";
+}
+
+void soln2(){ // stack based solution
+    int n;
+    cin >> n;
+    vector<int> p(n), a(n);
+    for(int i=0;i<n;i++){
+        cin >> p[i];
+    }
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+
+    // Step 1: Reduce 'a' using a stack to remove consecutive duplicates
+    stack<int> s;
+    for (int i = 0; i < n; ++i) {
+        if (s.empty() || s.top() != a[i]) {
+            s.push(a[i]);
+        }
+    }
+
+    // Step 2: Match the stack against 'p' from right to left
+    // Because the stack is LIFO, the top of the stack is the LAST unique element of 'a'
+    int p_ptr = n - 1;
+    while (p_ptr >= 0 && !s.empty()) {
+        if (p[p_ptr] == s.top()) {
+            s.pop(); // Found a match for the current required element
+        }
+        p_ptr--; // Move backwards through the permutation
+    }
+
+    // If the stack is empty, we matched all required elements in order
+    if (s.empty()) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -58,6 +116,10 @@ int main(){
     int t;
     cin >> t;
     while(t--)
-        soln();
+        // soln();
+        // soln1();
+        soln2();
+
+
     return 0;
 }
