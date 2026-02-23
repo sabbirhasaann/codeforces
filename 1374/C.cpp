573c0dfdb41ed322d8c83d016a695f67cc957c3b
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-23 19:15:24 +0600

    upsolved

---
+++ 1374/C.cpp
@@ -0,0 +1,41 @@
+// 2026/02/23 19:09:39
+// https://codeforces.com/problemset/problem/1374/C
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+void soln()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+
+    int balance = 0, moves = 0;
+    for(int i=0;i<n;++i)
+    {
+        if(s[i]==')')
+            --balance;
+        else
+            ++balance;
+
+        if(balance<0){
+            ++moves;
+            balance = 0;
+        }
+    }
+    cout << moves << endl;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
