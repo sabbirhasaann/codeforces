434a3705252c2ef607e7acc67de4b405b83b6615
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-02-24 20:06:06 +0600

    other soln

--- 0500/A.cpp
+++ 0500/A.cpp
@@ -7,6 +7,14 @@ using namespace std;
 #define YES cout << "YES\n"
 #define NO cout << "NO\n"
 #define endl '\n'
+
+const int N_ = 30500;
+int N, T, A[N_];
+
+int b[1234567];
+
+
+
 void soln()
 {
     int n,t;
@@ -42,8 +50,35 @@ void soln()
         YES;
     else
         NO;
+}
 
+void soln1(){
+    cin >> N >> T;
+    for(int i = 1; i < N; i++) {
+        cin >> A[i];
+    }
+    A[N] = 1;
+    for(int cur = 1; cur <= N; cur += A[cur]) {
+        if(cur == T){
+            cout << "YES";
+            return;
+        } 
+    }
+    
+    cout << "NO";
+}
 
+void soln2(){
+    int n, t;
+    cin >> n >> t;
+    for (int i = 1; i < n; i++) {
+        cin >> b[i];
+    }
+    int x = 1;
+    while (x < t) {
+        x += b[x];
+    }
+    x == t ? cout << "YES" : cout << "NO";
 }
 
 
@@ -53,6 +88,9 @@ int main(){
     int t=1;
     // cin >> t;
     while(t--)
-        soln();
+        // soln();
+        soln1();
+        // soln2();
+
     return 0;
 }
