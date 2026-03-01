38d306e624162711cd52aeb7b09a27eaa0daf9fa
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-01 09:33:37 +0600

    solved 1342A. Road To Zero

---
+++ 1342/A.cpp
@@ -0,0 +1,44 @@
+// 2026/03/01 09:17:32
+// https://codeforces.com/problemset/problem/1342/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+void soln()
+{
+    long long x,y,a,b,cost=0;
+    cin >> x >> y >> a >> b;
+
+    if((x>0 && y>0) || (x<0 && y<0)){
+        x = abs(x);
+        y = abs(y);
+
+        if(2 * a < b){
+            cost += x * a + y * a;
+        }
+        else{
+            cost += min(x,y) * b;
+            cost += (max(x,y) - min(x,y)) * a;
+        }
+    }
+
+    else{
+        cost += abs(x)*a + abs(y)*a;
+    }
+
+    cout << cost << endl;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
