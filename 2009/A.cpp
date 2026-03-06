01ebf72e1947a295d859d3b7395f0e023df79d08
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-06 09:57:50 +0600

    solved, takses 2+ minute(min) 2009A. Minimize

---
+++ 2009/A.cpp
@@ -0,0 +1,26 @@
+// 2026/03/06 09:55:40
+// https://codeforces.com/problemset/problem/2009/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+void soln()
+{
+    int a,b;
+    cin >> a >> b;
+    cout << b - a << endl;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
