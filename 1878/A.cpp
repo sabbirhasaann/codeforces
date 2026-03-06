1b04b177dd3e4fa0430026f28ece677aa1eb07cc
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-06 09:20:43 +0600

    effsoln

--- 1878/A.cpp
+++ 1878/A.cpp
@@ -33,6 +33,19 @@ void soln()
     NO;
 }
 
+void effsoln(){
+    int n, k;
+    cin >> n >> k;
+    bool ys=0; 
+    for(int i=0; i< n; i++){
+        int a; 
+        cin >> a;
+        if(a==k)ys=1; 
+    }
+    if(ys)cout << "YES\n";
+    else cout << "NO\n";
+}
+
 
 int main(){
     ios_base::sync_with_stdio(false);
