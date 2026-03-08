1b1e826c50cb3178cf0a4a26cbfecdceab511825
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-08 09:45:07 +0600

    effsoln

--- 1862/B.cpp
+++ 1862/B.cpp
@@ -33,6 +33,24 @@ void soln()
     cout << endl;
 }
 
+void effsoln(){
+    int n;
+    cin >> n;
+    vector<int> a;
+    for (int i = 0; i < n; ++i) {
+        int x;
+        cin >> x;
+        if (i && a.back() > x) {
+            a.push_back(1);
+        }
+        a.push_back(x);
+    }
+    cout << a.size() << "\n";
+    for (int el : a)
+        cout << el << " ";
+    cout << "\n";
+}
+
 
 int main(){
     ios_base::sync_with_stdio(false);
