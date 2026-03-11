30a7c1fd4d468a23c668aff34067825ffa41b80f
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-11 13:32:06 +0600

    effsoln

--- 0431/A.cpp
+++ 0431/A.cpp
@@ -20,6 +20,17 @@ void soln()
     cout << f[1] * a + f[2] * b + f[3] * c + f[4] * d;
 }
 
+void effsoln(){
+    int a[5];
+    for (int i = 1; i <= 4; i ++)
+        cin >> a[i];
+    string s;
+    cin >> s;
+    int sum = 0;
+    for (int i = 0 ;i < s.size(); i ++)
+        sum += a[s[i]-'0'];
+    cout << sum << endl;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
