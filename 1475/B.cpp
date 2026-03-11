0a05de23ef51f881d15ef246264e42a762b51b60
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-11 15:56:31 +0600

    another solution

--- 1475/B.cpp
+++ 1475/B.cpp
@@ -21,6 +21,17 @@ void soln()
 
 }
 
+void soln1(){
+    int n;
+    cin >> n;
+    int cnt2021 = n % 2020;
+    int cnt2020 = (n - cnt2021) / 2020 - cnt2021;
+    if (cnt2020 >= 0 && 2020 * cnt2020 + 2021 * cnt2021 == n) {
+      cout << "YES\n";
+    } else {
+      cout << "NO\n";
+    }
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
