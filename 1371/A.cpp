411601f7d25bccf894165590f83756c6663bcd0e
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-11 11:41:11 +0600

    solved 1371A. Magical Sticks

---
+++ 1371/A.cpp
@@ -0,0 +1,26 @@
+// 2026/03/11 11:39:11
+// https://codeforces.com/problemset/problem/1371/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+void soln()
+{
+    int n;
+    cin >> n;
+    cout << (n+1)/2 << endl;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
