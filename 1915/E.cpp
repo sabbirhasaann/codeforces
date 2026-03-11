012a02e15f4c171ba1378d0fbc74a922b2e3c286
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-11 16:35:40 +0600

    added link,in-out

--- 1915/E.cpp
+++ 1915/E.cpp
@@ -1,3 +1,5 @@
+// https://codeforces.com/problemset/problem/1915/E
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -31,3 +33,28 @@ int main()
     while(t--)
         solve();
 }
+
+/*input
+6
+3
+1 3 2
+6
+1 1 1 1 1 1
+10
+1 6 9 8 55 3 14 2 7 2
+8
+1 2 11 4 1 5 1 2
+6
+2 6 1 5 7 8
+9
+2 5 10 4 4 9 6 7 8
+*/
+
+/*output
+YES
+YES
+NO
+YES
+NO
+YES
+*/
