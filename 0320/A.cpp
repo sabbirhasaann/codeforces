4485503f00b0e07f33be68d06514260446e23b14
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-13 15:32:05 +0600

    another solution

--- 0320/A.cpp
+++ 0320/A.cpp
@@ -29,6 +29,30 @@ void soln()
 }
 
 
+bool is_magical(string number) {
+	for (int i = 0; i < (int)number.size(); i++)
+		if (number[i] != '1' && number[i] != '4')
+			return false;
+
+	if (number[0] == '4')
+		return false;
+
+	if (number.find("444") != number.npos)
+		return false;
+
+	return true;
+}
+
+void soln1() {
+	string number;
+	cin >> number;
+
+	if (is_magical(number))
+		cout << "YES" << endl;
+	else
+		cout << "NO" << endl;
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
 
