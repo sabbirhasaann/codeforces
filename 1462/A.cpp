e60797a2886f908fb8496c8a304387e22c25c506
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-14 10:30:57 +0600

    solved

---
+++ 1462/A.cpp
@@ -0,0 +1,78 @@
+// 2026/03/14 10:23:59
+// https://codeforces.com/problemset/problem/1462/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+
+void printArray(vector<int> &v)
+{
+    for (int x : v)
+        cout << x << " ";
+    cout << endl;
+}
+
+void soln()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n), b(n);
+    for(int i=0;i<n;++i)
+        cin >> a[i];
+
+    int i=0,j=0;
+    while(j<n){
+        b[j] = a[i];
+        ++i;
+        j+=2;
+    }
+    i = n - 1,j=1;
+    while(j<n){
+        b[j] = a[i];
+        --i;
+        j+=2;
+    }
+
+    printArray(b);
+
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
+
+
+/*input
+6
+7
+3 4 5 2 9 1 1
+4
+9 2 7 1
+11
+8 4 3 1 2 7 8 7 9 4 2
+1
+42
+2
+11 7
+8
+1 1 1 1 1 1 1 1
+*/
+
+/*output
+3 1 4 1 5 9 2 
+9 1 2 7 
+8 2 4 4 3 9 1 7 2 8 7 
+42 
+11 7 
+1 1 1 1 1 1 1 1 
+*/
