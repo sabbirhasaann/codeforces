29376ff9c1614f3c6abd74dfbf13167467046597
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-14 10:09:39 +0600

    another solution

--- 1941/A.cpp
+++ 1941/A.cpp
@@ -26,6 +26,24 @@ void soln()
     
 }
 
+void soln1(){
+    int n, m, k;
+    cin >> n >> m >> k;
+    int ans = 0;
+    vector<int> v1(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v1[i];
+    }
+    int o;
+    for (int j = 0; j < m; j++) {
+        cin >> o;
+        for (int i = 0; i < n; i++) {
+            if ((o + v1[i]) <= k) ans++;
+        }
+    }
+    cout << ans << "\n";
+}
+
 
 int main(){
     ios_base::sync_with_stdio(false);
