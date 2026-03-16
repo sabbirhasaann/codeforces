6c188f856395b6de3ddee7472b26c33eb79e5967
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-17 02:31:09 +0600

    effsoln

--- 1783/A.cpp
+++ 1783/A.cpp
@@ -32,6 +32,24 @@ void soln()
         NO;
 }
 
+void effsoln(){
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for(int i=0;i<n;++i)
+        cin >> a[i];
+    sort(a.begin(), a.end());
+    if(a[0]==a[n-1])
+        NO;
+    else{
+        YES;
+        cout << a[n-1] <<" ";
+        for(int i=0;i<n-1;++i)
+            cout << a[i] <<" ";
+        cout << endl;
+    }
+
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
@@ -39,7 +57,9 @@ int main(){
     int t;
     cin >> t;
     while(t--)
-        soln();
+        // soln();
+        effsoln();
+
     return 0;
 }
 
