29a67914c6bb5979ca5b819639181f75ae6a9d85
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-18 14:12:27 +0600

    effsoln

--- 1950/C.cpp
+++ 1950/C.cpp
@@ -26,6 +26,14 @@ void soln()
 }
 
 
+void effsoln() {
+    int h, m; char c;
+    cin >> h >> c >> m;
+    string am = (h < 12 ? " AM" : " PM");
+    h = (h % 12 ? h % 12 : 12);
+    cout << (h < 10 ? "0" : "") << h << c << (m < 10 ? "0" : "") << m << am << '\n';
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
 
