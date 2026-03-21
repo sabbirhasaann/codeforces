db01633a73b94f534b7e1ef955cdec09fdca63b5
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-21 17:58:59 +0600

    concise solution

--- 1311/A.cpp
+++ 1311/A.cpp
@@ -33,6 +33,12 @@ void soln()
     
 }
 
+void soln1(){
+    int a, b;
+    cin >> a >> b;
+    if (a == b) cout << 0 << endl;
+    else cout << 1 + int((a < b) ^ ((b - a) & 1)) << endl;
+}
 
 
 int main(){
