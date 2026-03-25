f8c35b09979d160d1391d2b295baf7876c522e06
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-25 09:57:51 +0600

    effsoln

--- 1294/A.cpp
+++ 1294/A.cpp
@@ -29,6 +29,17 @@ void soln()
 
 }
 
+void soln1(){
+    int a[3], n;
+    cin >> a[0] >> a[1] >> a[2] >> n;
+    sort(a, a + 3);
+    n -= 2 * a[2] - a[1] - a[0];
+    if (n < 0 || n % 3 != 0) {
+        cout << "NO" << endl;
+    } else {
+        cout << "YES" << endl;
+    }
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
