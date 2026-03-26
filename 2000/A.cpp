854f8a48bb64a993fc309514b6201be7d6ad358f
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-26 19:30:03 +0600

    ac

---
+++ 2000/A.cpp
@@ -0,0 +1,55 @@
+// 2026/03/26 19:11:07
+// https://codeforces.com/problemset/problem/2000/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+void soln()
+{
+    string s;
+    cin >> s;
+    int n = s.length();
+    
+    if(n == 3 && s[0]=='1' && s[1]=='0' && s[2] >= '2')
+        YES;
+    
+    else if(n>=4 && s[0]=='1' && s[1]=='0' && s[2] >= '1')
+        YES;
+    else
+        NO;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
+
+/*in
+7
+100
+1010
+101
+105
+2033
+1019
+1002
+*/
+
+/*out
+NO
+YES
+NO
+YES
+NO
+YES
+NO
+*/
