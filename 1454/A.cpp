d5279a22836f2de8871cd5a85002737746823398
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-26 19:58:53 +0600

    effsoln

--- 1454/A.cpp
+++ 1454/A.cpp
@@ -29,6 +29,14 @@ void soln()
 }
 
 
+void effsoln(){
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        cout << (i + 1) % n + 1 << " ";
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
 
