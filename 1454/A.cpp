32f5c111c72456afabc2b3531c65f79b2ca375d3
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-26 19:57:57 +0600

    ac

---
+++ 1454/A.cpp
@@ -0,0 +1,51 @@
+// 2026/03/26 19:34:10
+// https://codeforces.com/problemset/problem/1454/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+void soln()
+{
+    int n,mid;
+    cin >> n;
+    
+    if(n&1)
+        mid = (n+1)/2;
+    else
+        mid = n;
+
+    cout << mid << " ";
+    while (n)
+    {
+        if(n!=mid)
+            cout << n <<" ";
+        --n;
+    }
+    
+    cout << endl;
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
+
+/*in
+2
+2
+5
+*/
+
+/*out
+2 1 
+3 5 4 2 1 
+*/
