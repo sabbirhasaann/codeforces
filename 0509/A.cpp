e896bc3ae5f3d252a3fb444a38af52f5bdae2cd0
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-26 20:08:57 +0600

    ac

---
+++ 0509/A.cpp
@@ -0,0 +1,38 @@
+// 2026/03/26 20:02:07
+// https://codeforces.com/problemset/problem/509/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+void soln()
+{
+    int n;
+    cin >> n;
+
+    vector<vector<int>> mat(n, vector<int>(n,0));
+    for(int i=0;i<n;++i)
+        mat[i][0] = 1;
+    for(int i=0;i<n;++i)
+        mat[0][i] = 1;
+    
+    for(int i=1;i<n;++i)
+        for(int j=1;j<n;++j)
+            mat[i][j] = mat[i-1][j] + mat[i][j-1];
+
+    cout << mat[n-1][n-1] << endl;
+    
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(false);
+
+    int t=1;
+    // cin >> t;
+    while(t--)
+        soln();
+    return 0;
+}
