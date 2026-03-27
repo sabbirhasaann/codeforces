955a6233b35d7edb3065e1c8be06f001c23d11a2
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-27 08:37:33 +0600

    clean soln

--- 1856/A.cpp
+++ 1856/A.cpp
@@ -25,6 +25,23 @@ void soln()
     cout << ops << endl;
 }
 
+void soln1() {
+    int n;
+    cin >> n;
+    
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    
+    int ans = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (a[i] > a[i + 1]) {
+            ans = max(ans, a[i]);
+        }
+    }
+    
+    cout << ans << endl;
+}
+
 
 int main(){
     ios_base::sync_with_stdio(false);
