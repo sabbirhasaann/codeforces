662a7bfabb1476f656aa9b3787379db092f4a3af
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-27 12:49:10 +0600

    effsoln

--- 2114/A.cpp
+++ 2114/A.cpp
@@ -36,6 +36,17 @@ void soln()
         cout << k << endl;
 }
 
+void solve() {
+    int n;
+    cin >> n;
+    int sq = ceil(sqrt(n));
+    if (sq * sq == n) {
+        cout << 0 << ' ' << sq << "\n";
+    } else {
+        cout << "-1\n";
+    }
+}
+
 
 int main(){
     ios_base::sync_with_stdio(false);
