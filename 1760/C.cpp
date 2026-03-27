9856ecbf2eeadaf3cd25021d3609eebfd319d9b6
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-27 07:56:01 +0600

    another soln

--- 1760/C.cpp
+++ 1760/C.cpp
@@ -39,18 +39,19 @@ void soln()
     }    
 }
 
-void soln1(){ // ordered not maintained
+void soln1(){
     int n;
     cin >> n;
     vector<int> arr(n);
     for(int i=0;i<n;i++)
         cin >> arr[i];
-    sort(arr.begin(), arr.end());
+    vector<int> a(arr);
+    sort(a.begin(), a.end());
     for(int i=0;i<n;++i){
-        if(arr[i]==arr[n-1])
-            cout << arr[i] - arr[n-2] <<" ";
+        if(arr[i]==a[n-1])
+            cout << arr[i] - a[n-2] <<" ";
         else
-            cout << arr[i] - arr[n-1] << " ";
+            cout << arr[i] - a[n-1] << " ";
     }
     cout << endl;
 }
@@ -62,8 +63,8 @@ int main(){
     int t;
     cin >> t;
     while(t--)
-        soln();
-        // soln1();
+        // soln();
+        soln1();
 
     return 0;
 }
