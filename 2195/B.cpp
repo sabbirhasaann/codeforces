85f9fabcd3ff510c06e654b1bcd8c534e97b9209
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-03-29 21:42:30 +0600

    another soln

--- 2195/B.cpp
+++ 2195/B.cpp
@@ -58,6 +58,27 @@ void soln1(){
     cout << "YES\n";
 }
 
+void soln2(){
+    int n;cin>>n;
+    vector<int>a(n+1);
+    for(int i=1;i<=n;i++)
+    {
+      cin>>a[i];
+    }
+    for(int i=1;i<=n;i+=2)
+    {
+      for(int j=i;j<=n;j*=2)
+      {
+        for(int k=i*2;k<=n;k*=2)
+        {
+          if(a[k/2]>a[k])swap(a[k/2],a[k]);
+        }
+      }
+    }
+    if(is_sorted(begin(a),end(a)))cout<<"YES\n";
+    else cout<<"NO\n";
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
 
