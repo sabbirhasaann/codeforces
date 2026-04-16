daa39bf56258b3220bc4274691275614078f6460
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-04-16 17:30:51 +0600

    another soln

--- 1985/D.cpp
+++ 1985/D.cpp
@@ -55,6 +55,26 @@ void soln()
     
 }   
 
+void soln1(){
+    int n, m; cin >> n >> m;
+    vector<vector<char>> g(n, vector<char>(m));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> g[i][j];
+        }
+    }
+    pair<int, int> top = {INF, INF}, bottom = {-INF, -INF};
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(g[i][j] == '#'){
+                top = min(top, {i, j});
+                bottom = max(bottom, {i, j});
+            }
+        }
+    }
+    assert(top.second == bottom.second);
+    cout << (top.first + bottom.first) / 2 + 1 << " " << top.second + 1 << endl;
+}
 
 int main(){
     ios_base::sync_with_stdio(false);
