74cda6a8569c6dce23653dd0344b62f1c1f87335
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-05-08 07:01:12 +0600

    solved

---
+++ 1339/A.cpp
@@ -0,0 +1,38 @@
+// 2026/04/25 21:13:48
+// https://codeforces.com/problemset/problem/1339/A
+
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define YES cout << "YES\n"
+#define NO cout << "NO\n"
+#define endl '\n'
+vector<long long> ans();
+void soln()
+{
+    int n;
+    cin >> n;
+    cout << n << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+
+    int t;
+    cin >> t;
+    while (t--)
+        soln();
+    return 0;
+}
+
+/*in
+2
+2
+1
+*/
+
+/*out
+2
+1
+*/
