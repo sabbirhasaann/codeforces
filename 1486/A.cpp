cc9c00420c6b81ca4b7619fe1b837cac4e715c51
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-05-16 07:52:29 +0600

    another soluntion 1486A. Shifting Blocks

--- 1486/A.cpp
+++ 1486/A.cpp
@@ -31,6 +31,23 @@ void soln()
         NO;
 }
 
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto &i : a) cin >> i;
+    long long sum = 0, need = 0;
+    for (int i = 0; i < n; ++i) {
+        need += i;
+        sum += a[i];
+        if (sum < need) {
+            cout << "NO\n";
+            return;
+        }
+    }
+    cout << "YES\n";
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
