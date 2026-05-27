9aa9916fb34bc30fe4a9dfb6abe6b00273392d83
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-05-27 06:21:59 +0600

    another soln1 1613A. Long Comparison

--- 1613/A.cpp
+++ 1613/A.cpp
@@ -59,6 +59,33 @@ void soln()
     cout << endl;
 }
 
+void soln1()
+{
+    long long x1, x2;
+    int p1, p2;
+    cin >> x1 >> p1 >> x2 >> p2;
+    int mn = min(p1, p2);
+    p1 -= mn;
+    p2 -= mn;
+    if (p1 >= 7)
+        cout << ">" << endl;
+    else if (p2 >= 7)
+        cout << "<" << endl;
+    else
+    {
+        for (int i = 0; i < p1; ++i)
+            x1 *= 10;
+        for (int i = 0; i < p2; ++i)
+            x2 *= 10;
+        if (x1 < x2)
+            cout << "<" << endl;
+        else if (x1 > x2)
+            cout << ">" << endl;
+        else
+            cout << "=" << endl;
+    }
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
