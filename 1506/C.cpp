36453db69926f8afeac53d0bcf50b7acf464db3a
sabbirhasaann <sabbirhasan9989@gmail.com>
2026-07-06 08:19:11 +0600

    soln1 - 1506C. Double-ended Strings

--- 1506/C.cpp
+++ 1506/C.cpp
@@ -42,6 +42,28 @@ void soln()
     cout << a.length() + b.length() - 2 * maxi << endl;
 }
 
+void solve()
+{
+    string a, b;
+    cin >> a >> b;
+    int n = a.size(), m = b.size();
+    int ans = 0;
+    for (int len = 1; len <= min(n, m); len++)
+    {
+        for (int i = 0; i + len <= n; i++)
+        {
+            for (int j = 0; j + len <= m; j++)
+            {
+                if (a.substr(i, len) == b.substr(j, len))
+                {
+                    ans = max(ans, len);
+                }
+            }
+        }
+    }
+    cout << a.size() + b.size() - 2 * ans << "\n";
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
